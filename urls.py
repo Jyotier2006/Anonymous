28c9161cf31c099bea515c45cf543f76fcec8f0a
Jyotier Vithalani <jyotier.v@ahduni.edu.in>
2026-01-18 13:19:04 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -13,4 +13,17 @@ urlpatterns = [
     path("educator/", views.educator_dashboard, name="educator_dashboard"),
     path("topic/status/", views.update_topic_status, name="update_topic_status"),
     path('dashboard/', views.home_view, name='dashboard'),
+    path("skills/", views.skills_page, name="skills_page"),
+    path("skills/delete/<int:skill_id>/", views.delete_skill, name="delete_skill"),
+    path("courses/", views.courses_page, name="courses_page"),
+    path("courses/delete/<int:course_id>/", views.delete_course, name="delete_course"),
+    path("projects/", views.projects_page, name="projects_page"),
+    path("projects/delete/<int:project_id>/", views.delete_project, name="delete_project"),
+    path("achievements/", views.achievements_page, name="achievements_page"),
+    path("achievements/delete/<int:ach_id>/", views.delete_achievement, name="delete_achievement"),
+    path("advisor/", views.ai_advisor_page, name="ai_advisor_page"),
+    path("student/", views.student_dashboard, name="student_dashboard"),
+    path("advisor/", views.ai_advisor_page, name="ai_advisor"),
+    path("advisor/chat/", views.ai_advisor_chat, name="ai_advisor_chat"),
+
 ]
