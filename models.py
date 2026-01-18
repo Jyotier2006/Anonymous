ad89adc1bde3ad797859735b85c5c74b79e95bf2
Jyotier Vithalani <jyotier.v@ahduni.edu.in>
2026-01-18 13:18:17 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 DOMAIN_CHOICES = [("healthcare", "Healthcare")]
-ROLE_CHOICES = [("student","Student"), ("educator","Educator")]
+ROLE_CHOICES = [("student", "Student"), ("educator", "Educator")]
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,8 +12,7 @@ class Profile(models.Model):
     headline = models.CharField(max_length=120, blank=True)
     bio = models.TextField(blank=True)
     target_role = models.CharField(max_length=120, blank=True)
-
-
+    github_username = models.CharField(max_length=39, blank=True, default="")
     def __str__(self):
         return f"{self.user.username} Profile"
 
@@ -45,7 +45,7 @@ class UserProject(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=140)
     description = models.TextField(blank=True)
-    tools = models.CharField(max_length=160, blank=True)  # SQL, Excel, Python...
+    tools = models.CharField(max_length=160, blank=True)  # Holistic Skill Intelligence System, Excel, Python...
     link = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -58,6 +58,11 @@ class UserAchievement(models.Model):
     title = models.CharField(max_length=160)
     date = models.DateField(null=True, blank=True)
     proof_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
+
+
 class LearningTopic(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics_created")
     domain = models.CharField(max_length=50, default="healthcare")
@@ -82,6 +87,5 @@ class TopicProgress(models.Model):
     class Meta:
         unique_together = ("student", "topic")
 
-
     def __str__(self):
-        return f"{self.user.username} - {self.title}"
+        return f"{self.student.username} - {self.topic.title} ({self.status})"
